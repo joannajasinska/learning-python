388b451f6f761ebbbb23f0b1711e072148837d88
Joanna Jasinska <wisegirl1808@gmail.com>
2021-03-28 21:22:24 +0200

    Nauka Macierzy

---
+++ listy.py
@@ -0,0 +1,14 @@
+lista = [1, "cos"]
+
+lista.append(3.4)
+print(lista)
+
+macierz =[
+    ["a","b","c"],
+    [1,2,3]
+]
+print(macierz[0][1])
+for wiersz in macierz:
+    for element in wiersz:
+        print(element, end= '') 
+
