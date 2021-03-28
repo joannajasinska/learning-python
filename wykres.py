86b852e29d507dd41bdc622d8c5081430d7f9601
Joanna Jasinska <wisegirl1808@gmail.com>
2021-03-28 23:11:27 +0200

    Narysowanie Wykresu Zmian Temperatury

--- wykres.py
+++ wykres.py
@@ -1,6 +1,18 @@
 import sys
 import csv
+import matplotlib.pyplot as plt
+from dateutil.parser import parse
 file= open("dane.csv", "r")
 iterator = csv.reader(file, delimiter= ",")
 lista = list(iterator)
 print(lista)
+plt.subplot(2,1,1)
+czas=[]
+temperatura=[]
+for element in lista:
+    czas.append(parse(element[0]))
+    temperatura.append(float(element[1]))
+
+plt.plot(czas, temperatura)
+plt.show()
+
